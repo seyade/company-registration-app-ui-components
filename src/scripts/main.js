10f7881d782d37e1fa956c03132705b6104900d0
Sey <olusegun.adegun@ibm.com>
2018-12-09 13:03:51 +0400

    converted vanilla JS to ES6

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,44 @@
 console.log('MAIN IS RUNNING!!!!');
-(function() {
-  const toggleButtons = document.querySelectorAll('.button--toggle');
 
-  for (let toggleButton of toggleButtons) {
-    toggleButton.addEventListener('click', event => {
-      let parentGroup = event.currentTarget.parentElement;
-      let details = parentGroup.nextElementSibling;
+class App {
+  constructor() {
+    /**
+     * @description buttons to toggle content of Path Summary
+     */
+    this.toggleButtons = document.querySelectorAll('.button--toggle');
 
-      if (details.classList.contains('path-summary__details--hidden')) {
-        details.classList.remove('path-summary__details--hidden');
-        event.currentTarget.classList.add('button--toggle--open');
-      } else {
-        details.classList.add('path-summary__details--hidden');
-        event.currentTarget.classList.remove('button--toggle--open');
-      }
-    });
+    /**
+     * @description button to close panel
+     */
+    this.closeButton = document.querySelector('.button--close');
   }
 
-  const closeButton = document.querySelector('.button--close');
+  toggle() {
+    for (let toggleButton of this.toggleButtons) {
+      toggleButton.addEventListener('click', event => {
+        let parentGroup = event.currentTarget.parentElement;
+        let details = parentGroup.nextElementSibling;
 
-  closeButton.addEventListener('click', event => {
-    event.currentTarget.parentElement.classList.add('announcement--hidden');
-  });
-})();
+        if (details.classList.contains('path-summary__details--hidden')) {
+          details.classList.remove('path-summary__details--hidden');
+          event.currentTarget.classList.add('button--toggle--open');
+        } else {
+          details.classList.add('path-summary__details--hidden');
+          event.currentTarget.classList.remove('button--toggle--open');
+        }
+      });
+    }
+  }
+
+  closePanel() {
+    this.closeButton.addEventListener('click', event => {
+      event.currentTarget.parentElement.classList.add('announcement--hidden');
+    });
+  }
+}
+
+// start the app
+const app = new App();
+
+app.toggle();
+app.closePanel();
