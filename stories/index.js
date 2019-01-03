f20fde6063d000d60e026047feb77dab475fa98c
Sey <olusegun.adegun@ibm.com>
2019-01-03 11:52:59 +0400

    DIFC-52: created progress-bar component "progress-bar--a.html"

--- stories/index.js
+++ stories/index.js
@@ -816,20 +816,47 @@ storiesOf('DIFC Components/Progress Indicators', module)
   `
   );
 
+storiesOf('DIFC Components/Progress Bars', module).add(
+  'Base with descriptions',
+  () => `
+    <div class="progress-bar" style="padding: 16px;">
+      <div
+        class="slds-grid slds-grid_align-spread slds-p-bottom_large"
+        id="progress-bar-label-id-1"
+      >
+        <strong>Your pathway</strong>
+        <span aria-hidden="true"><strong>25% Complete</strong></span>
+      </div>
+      <div
+        class="slds-progress-bar slds-progress-bar_small"
+        aria-valuemin="0"
+        aria-valuemax="100"
+        aria-valuenow="25"
+        aria-labelledby="progress-bar-label-id-1"
+        role="progressbar"
+      >
+        <span class="slds-progress-bar__value" style="width: 25%;">
+          <span class="slds-assistive-text">Progress: 25%</span>
+        </span>
+      </div>
+    </div>
+  `
+);
+
 storiesOf('DIFC Components/Breadcrumb', module)
   .add(
     'Breadcrumb',
     () => `
-      <div class="demo-only" style="height: 150px;padding: 16px;">
-        <!-- below is the real component -->
-        <nav class="breadcrumb" role="navigation" aria-label="Breadcrumbs">
-          <ol class="slds-breadcrumb slds-list_horizontal slds-wrap">
-            <li class="slds-breadcrumb__item slds-text-title_caps"><a href="javascript:void(0);">Parent Name</a></li>
-            <li class="slds-breadcrumb__item slds-text-title_caps"><a href="javascript:void(0);">Child Name</a></li>
-          </ol>
-        </nav>
-      </div>
-      `
+    <div class="demo-only" style="height: 150px;padding: 16px;">
+      <!-- below is the real component -->
+      <nav class="breadcrumb" role="navigation" aria-label="Breadcrumbs">
+        <ol class="slds-breadcrumb slds-list_horizontal slds-wrap">
+          <li class="slds-breadcrumb__item slds-text-title_caps"><a href="javascript:void(0);">Parent Name</a></li>
+          <li class="slds-breadcrumb__item slds-text-title_caps"><a href="javascript:void(0);">Child Name</a></li>
+        </ol>
+      </nav>
+    </div>
+    `
   )
   .add(
     'Breadcrumb overflow',
