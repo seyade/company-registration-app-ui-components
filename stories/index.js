8a84b2d59e4121ba0c1867584faabcac9b816652
Sey <olusegun.adegun@ibm.com>
2019-01-07 11:16:02 +0400

    indented some code

--- stories/index.js
+++ stories/index.js
@@ -3920,55 +3920,55 @@ storiesOf('DIFC Components/Menu', module)
   .add(
     'Base',
     () => `
-  <div class="ui-menu">
-    <div class="slds-dropdown-trigger slds-dropdown-trigger_click slds-is-open">
-      <button
-        class="slds-button slds-button_icon"
-        aria-haspopup="true"
-        title="Show More"
-      >
-        <svg class="slds-button__icon" aria-hidden="true">
-          <use
-            xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#down"
-          ></use>
-        </svg>
-        <span class="slds-assistive-text">Show More</span>
-      </button>
-      <div class="slds-dropdown slds-dropdown_left">
-        <ul class="slds-dropdown__list" role="menu" aria-label="Show More">
-          <li class="slds-dropdown__item" role="presentation">
-            <a href="javascript:void(0);" role="menuitem" tabindex="0">
-              <span class="slds-truncate" title="Menu Item One">
-                Menu Item One
-              </span>
-            </a>
-          </li>
-          <li class="slds-dropdown__item" role="presentation">
-            <a href="javascript:void(0);" role="menuitem" tabindex="-1">
-              <span class="slds-truncate" title="Menu Item Two">
-                Menu Item Two
-              </span>
-            </a>
-          </li>
-          <li class="slds-dropdown__item" role="presentation">
-            <a href="javascript:void(0);" role="menuitem" tabindex="-1">
-              <span class="slds-truncate" title="Menu Item Three">
-                Menu Item Three
-              </span>
-            </a>
-          </li>
-          <li class="slds-has-divider_top-space" role="separator"></li>
-          <li class="slds-dropdown__item" role="presentation">
-            <a href="javascript:void(0);" role="menuitem" tabindex="-1">
-              <span class="slds-truncate" title="Menu Item Four">
-                Menu Item Four
-              </span>
-            </a>
-          </li>
-        </ul>
+    <div class="ui-menu">
+      <div class="slds-dropdown-trigger slds-dropdown-trigger_click slds-is-open">
+        <button
+          class="slds-button slds-button_icon"
+          aria-haspopup="true"
+          title="Show More"
+        >
+          <svg class="slds-button__icon" aria-hidden="true">
+            <use
+              xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#down"
+            ></use>
+          </svg>
+          <span class="slds-assistive-text">Show More</span>
+        </button>
+        <div class="slds-dropdown slds-dropdown_left">
+          <ul class="slds-dropdown__list" role="menu" aria-label="Show More">
+            <li class="slds-dropdown__item" role="presentation">
+              <a href="javascript:void(0);" role="menuitem" tabindex="0">
+                <span class="slds-truncate" title="Menu Item One">
+                  Menu Item One
+                </span>
+              </a>
+            </li>
+            <li class="slds-dropdown__item" role="presentation">
+              <a href="javascript:void(0);" role="menuitem" tabindex="-1">
+                <span class="slds-truncate" title="Menu Item Two">
+                  Menu Item Two
+                </span>
+              </a>
+            </li>
+            <li class="slds-dropdown__item" role="presentation">
+              <a href="javascript:void(0);" role="menuitem" tabindex="-1">
+                <span class="slds-truncate" title="Menu Item Three">
+                  Menu Item Three
+                </span>
+              </a>
+            </li>
+            <li class="slds-has-divider_top-space" role="separator"></li>
+            <li class="slds-dropdown__item" role="presentation">
+              <a href="javascript:void(0);" role="menuitem" tabindex="-1">
+                <span class="slds-truncate" title="Menu Item Four">
+                  Menu Item Four
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
   `
   )
   .add(
@@ -4245,6 +4245,6 @@ storiesOf('DIFC Components/Menu', module)
           </ul>
         </div>
       </div>
-</div>
+    </div>
   `
   );
