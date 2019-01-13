d361e861e38463364d0069892200aee26a1a6b2f
Sey <olusegun.adegun@ibm.com>
2019-01-13 14:36:29 +0400

    DIFC-38: added tab-bar to storybook

--- stories/index.js
+++ stories/index.js
@@ -4529,6 +4529,172 @@ storiesOf('DIFC Components/Progress Rings', module)
     `
   );
 
+storiesOf('DIFC Components/Tab Bars', module)
+  .add(
+    'Base',
+    () => `
+    <div class="ui-tab-bar">
+      <div class="slds-tabs_default">
+        <ul class="slds-tabs_default__nav" role="tablist">
+          <li
+            class="slds-tabs_default__item slds-is-active"
+            title="Item One"
+            role="presentation"
+          >
+            <a
+              class="slds-tabs_default__link"
+              href="#"
+              role="tab"
+              tabindex="0"
+              aria-selected="true"
+              aria-controls="tab-default-1"
+              id="tab-default-1__item"
+            >
+              Item One
+            </a>
+          </li>
+          <li class="slds-tabs_default__item" title="Item Two" role="presentation">
+            <a
+              class="slds-tabs_default__link"
+              href="#"
+              role="tab"
+              tabindex="-1"
+              aria-selected="false"
+              aria-controls="tab-default-2"
+              id="tab-default-2__item"
+            >
+              Item Two
+            </a>
+          </li>
+          <li
+            class="slds-tabs_default__item"
+            title="Item Three"
+            role="presentation"
+          >
+            <a
+              class="slds-tabs_default__link"
+              href="#"
+              role="tab"
+              tabindex="-1"
+              aria-selected="false"
+              aria-controls="tab-default-3"
+              id="tab-default-3__item"
+            >
+              Item Three
+            </a>
+          </li>
+        </ul>
+        <div
+          id="tab-default-1"
+          class="slds-tabs_default__content slds-show"
+          role="tabpanel"
+          aria-labelledby="tab-default-1__item"
+        >
+          Item One Content
+        </div>
+        <div
+          id="tab-default-2"
+          class="slds-tabs_default__content slds-hide"
+          role="tabpanel"
+          aria-labelledby="tab-default-2__item"
+        >
+          Item Two Content
+        </div>
+        <div
+          id="tab-default-3"
+          class="slds-tabs_default__content slds-hide"
+          role="tabpanel"
+          aria-labelledby="tab-default-3__item"
+        >
+          Item Three Content
+        </div>
+      </div>
+    </div>
+    `
+  )
+  .add(
+    'Long Text',
+    () => `
+    <div class="ui-tab-bar">
+      <div class="slds-tabs_default">
+        <ul class="slds-tabs_default__nav" role="tablist">
+          <li
+            class="slds-tabs_default__item slds-is-active"
+            title="Item One"
+            role="presentation"
+          >
+            <a
+              class="slds-tabs_default__link"
+              href="#"
+              role="tab"
+              tabindex="0"
+              aria-selected="true"
+              aria-controls="tab-default-1"
+              id="tab-default-1__item"
+            >
+              Item One long head
+            </a>
+          </li>
+          <li class="slds-tabs_default__item" title="Item Two" role="presentation">
+            <a
+              class="slds-tabs_default__link"
+              href="#"
+              role="tab"
+              tabindex="-1"
+              aria-selected="false"
+              aria-controls="tab-default-2"
+              id="tab-default-2__item"
+            >
+              Item Two long head
+            </a>
+          </li>
+          <li
+            class="slds-tabs_default__item"
+            title="Item Three"
+            role="presentation"
+          >
+            <a
+              class="slds-tabs_default__link"
+              href="#"
+              role="tab"
+              tabindex="-1"
+              aria-selected="false"
+              aria-controls="tab-default-3"
+              id="tab-default-3__item"
+            >
+              Item Three long head
+            </a>
+          </li>
+        </ul>
+        <div
+          id="tab-default-1"
+          class="slds-tabs_default__content slds-show"
+          role="tabpanel"
+          aria-labelledby="tab-default-1__item"
+        >
+          Item One Content
+        </div>
+        <div
+          id="tab-default-2"
+          class="slds-tabs_default__content slds-hide"
+          role="tabpanel"
+          aria-labelledby="tab-default-2__item"
+        >
+          Item Two Content
+        </div>
+        <div
+          id="tab-default-3"
+          class="slds-tabs_default__content slds-hide"
+          role="tabpanel"
+          aria-labelledby="tab-default-3__item"
+        >
+          Item Three Content
+        </div>
+      </div>
+    </div>
+    `
+  );
+
 storiesOf('DIFC Components/Tooltip', module).add(
   'Base',
   () => `
